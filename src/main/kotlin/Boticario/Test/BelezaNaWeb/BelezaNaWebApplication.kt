698ed2172b77a84c2c167fc413b99828a4984d9c
gisellenrosa <gisellenrosa@gmail.com>
2021-08-13 10:42:18 -0300

    Structure and dependencies

---
+++ src/main/kotlin/Boticario/Test/BelezaNaWeb/BelezaNaWebApplication.kt
@@ -0,0 +1,11 @@
+package Boticario.Test.BelezaNaWeb
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class BelezaNaWebApplication
+
+fun main(args: Array<String>) {
+	runApplication<BelezaNaWebApplication>(*args)
+}
---
+++ src/test/kotlin/Boticario/Test/BelezaNaWeb/BelezaNaWebApplicationTests.kt
@@ -0,0 +1,13 @@
+package Boticario.Test.BelezaNaWeb
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class BelezaNaWebApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
